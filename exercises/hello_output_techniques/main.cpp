b6bdbf7c2233d24d0ea042384a19be12431e81c2
Zeno <eng.tonyalc@gmail.com>
2024-02-16 20:39:35 -0300

    :memo: Output techniques - Coding Exercise

---
+++ exercises/hello_output_techniques/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+     // Method 1
+     cout << "Hello, world!" << endl;
+
+     // Method 2
+     cout << "Hello" << endl;
+     cout << "world!" << endl;
+
+     // Method 3
+     cout << "Hello "
+          << "world!" << endl;
+
+     // Method 4
+     cout << "Hello" << endl
+          << "world!" << endl;
+
+     // Method 5
+     cout << "Hello" << endl
+          << endl
+          << "world!" << endl;
+
+     // Method 6
+     cout << "Hello ";
+     cout << "world!" << endl;
+
+     return 0;
+}
