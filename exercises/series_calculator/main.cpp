2d3881eaaff273c1af554c575a62959100bb565f
Zeno <eng.tonyalc@gmail.com>
2024-03-19 16:24:38 -0300

    :recycle: Added sine series function

--- exercises/series_calculator/main.cpp
+++ exercises/series_calculator/main.cpp
@@ -2,11 +2,14 @@
   Taylor series
 
   e^x = 1 + x + x²/2! + x³/3! + x^4/4! + ...
+
+  sen(x) = x - x³/3! + x^5/5! + x^7/7! + ...
 */
 
 #include <iostream>
+#include <cmath>
 
-int main()
+void exponentialSeries()
 {
   double x;
   double e_to_x = 1.0;
@@ -25,6 +28,37 @@ int main()
   }
 
   std::cout << "The value of e^" << x << " using the series up to tenth element is: " << e_to_x << std::endl;
+}
+
+void sineSeries()
+{
+  double x;
+  double sin_to_x = 0.0;
+
+  std::cout << "Enter the value of 'x' (in rads): ";
+  std::cin >> x;
+
+  double pow_of_x = x;
+  double factorial = 1.0;
+  double term = pow_of_x;
+  int n = 0;
+
+  while (fabs(term) >= 0.001)
+  {
+    sin_to_x += term;
+    n++;
+    factorial *= 2 * n * (2 * n + 1);
+    pow_of_x *= std::pow(x, 2);
+    term = (pow_of_x / factorial) * ((n % 2 == 0) ? 1 : -1);
+  }
+
+  std::cout << "The value of sin(" << x << ") using the Taylor series is: " << sin_to_x << std::endl;
+}
+
+int main()
+{
+  exponentialSeries();
+  sineSeries();
 
   return 0;
 }
